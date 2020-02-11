112c3cd2a814b30076fc12b81052087955710e8c
codesteller <pallab.vsp@gmail.com>
2020-02-11 19:41:30 +0530

    bugfix: contrib checkout

--- docker_setup.sh
+++ docker_setup.sh
@@ -30,7 +30,11 @@ curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
-sudo systemctl restart docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+docker --version
 
 #### Test nvidia-smi with the latest official CUDA image
-docker run --gpus all nvidia/cuda:9.0-base nvidia-smi
+sudo docker run --gpus all nvidia/cuda:9.0-base nvidia-smi
+
--- opencv_setup.sh
+++ opencv_setup.sh
@@ -48,7 +48,8 @@ cd ..
 cd "$cwd"
  
 git clone https://github.com/opencv/opencv_contrib.git
-cd opencv_contrib 4.1.1
+cd opencv_contrib 
+git checkout 4.1.1
 cd ..
 
 cd "$cwd"
