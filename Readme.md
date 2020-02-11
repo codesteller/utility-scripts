1e2c4f76b59e6dee50a1c1e7a82c7c0ef6a52bec
codesteller <pallab.vsp@gmail.com>
2020-02-11 19:15:00 +0530

    added docker setup with NVIDIA containers

--- Readme.md
+++ Readme.md
@@ -2,4 +2,4 @@
 These scripts are intended to setup Ubuntu 18.04 system
 
 - opencv setup added - Version 4.1.1
-- docker ce setup added - for x86-64 system
+- docker ce setup added - for x86-64 system with NVidia Docker
--- docker_setup.sh
+++ docker_setup.sh
@@ -22,3 +22,15 @@ sudo apt-get install docker-ce
 
 # Uninstall Docker
 # sudo apt-get remove docker docker-engine docker.io
+
+# NVIDIA Docker for Ubuntu 16.04/18.04, Debian Jessie/Stretch/Buster
+# Add the package repositories
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo systemctl restart docker
+
+#### Test nvidia-smi with the latest official CUDA image
+docker run --gpus all nvidia/cuda:9.0-base nvidia-smi
