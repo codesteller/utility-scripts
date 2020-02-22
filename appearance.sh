2acdfde3df6999a9df830113bd2cbfadd4c89a34
Pallab Maji <5181716+codesteller@users.noreply.github.com>
2020-02-22 19:56:28 +0530

    added desktop environment list

--- appearance.sh
+++ appearance.sh
@@ -7,3 +7,9 @@ sudo apt install arc-theme
 # Reboot and launch gnome-tweaks to change to a dark theme
 # I prefer the ARC-DARK theme over ADWAITA-DARK
 # However, the ADWAITA-DARK theme is also good.
+# For some of the other themes, consider
+# https://www.omgubuntu.co.uk/2017/11/best-gtk-themes-for-ubuntu
+
+# I prefer the default desktop which is GNOME-DESKTOP. 
+# However, there are few others as mentioned in
+# https://linuxconfig.org/8-best-ubuntu-desktop-environments-18-04-bionic-beaver-linux
