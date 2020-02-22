066e31851d5247d3b1f8665e9daa8d5944c57f4b
Pallab Maji <5181716+codesteller@users.noreply.github.com>
2020-02-22 19:50:45 +0530

    update appearence with arc theme

--- appearance.sh
+++ appearance.sh
@@ -5,3 +5,5 @@ sudo apt-get install gnome-shell-extensions
 sudo apt install arc-theme
 
 # Reboot and launch gnome-tweaks to change to a dark theme
+# I prefer the ARC-DARK theme over ADWAITA-DARK
+# However, the ADWAITA-DARK theme is also good.
