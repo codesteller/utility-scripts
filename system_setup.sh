fcd03d11e21eac218f4a392f2d691c313c38f08b
Pallab Maji <5181716+codesteller@users.noreply.github.com>
2020-02-23 16:53:02 +0530

    added exfat installation

--- system_setup.sh
+++ system_setup.sh
@@ -30,3 +30,6 @@ curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
 sudo apt-get update
 sudo apt-get install sublime-text
+
+# For exfat install
+sudo apt-get install exfat-fuse exfat-utils
