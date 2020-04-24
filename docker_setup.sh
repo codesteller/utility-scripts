c47d927851015969f9a5610b61758661a1e8c003
Pallab Maji <5181716+codesteller@users.noreply.github.com>
2020-04-24 20:38:22 +0530

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -17,7 +17,7 @@ sudo apt-key fingerprint 0EBFCD88
 # Get Docker CE for Ubuntu
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 
 # Uninstall Docker
