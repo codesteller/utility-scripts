85c6fc956b3f3394d1c6020bb0b60d7a8df0f975
Pallab Maji <5181716+codesteller@users.noreply.github.com>
2020-05-28 19:09:33 +0530

    Update jetson-setup.sh

--- jetson-setup.sh
+++ jetson-setup.sh
@@ -1,2 +1,11 @@
 echo "export PATH=/usr/src/tensorrt/bin:$PATH" >> $HOME/.bashrc
 # TensorRT Samples stored in /opt
+
+
+# Prepare Xavier for the JetPack and maximize the performance with:
+sudo nvpmodel -m 0
+sudo jetson_clocks
+
+# Generate yolov3.onnx with the README shared in the /usr/src/tensorrt/samples/python/yolov3_onnx/.
+# Run the yolov3.onnx with trtexec, which targets for the profiling.
+/usr/src/tensorrt/bin/trtexec --onnx=./yolov3.onnx --workspace=26 --int8
