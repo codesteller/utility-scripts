06f5f5e1da486eaeea653c4abc2cda39aa5bdacc
Pallab Maji <5181716+codesteller@users.noreply.github.com>
2021-09-18 11:13:27 +0530

    Update jetson-setup.sh

--- jetson-setup.sh
+++ jetson-setup.sh
@@ -1,9 +1,13 @@
+sudo apt update && sudo apt upgrade -y
+sudo apt install python3-dev python3-pip python3-venv
+sudo -H pip3 install -U jetson-stats
+
 echo "export PATH=/usr/src/tensorrt/bin:$PATH" >> $HOME/.bashrc
 # TensorRT Samples stored in /opt
 
 
 # Prepare Xavier for the JetPack and maximize the performance with:
-sudo nvpmodel -m 0
+sudo nvpmodel -m 0 # Add the power mode as needed. To more about the power modes refer manual
 sudo jetson_clocks
 
 # Generate yolov3.onnx with the README shared in the /usr/src/tensorrt/samples/python/yolov3_onnx/.
