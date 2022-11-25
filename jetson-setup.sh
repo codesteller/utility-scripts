9c782ecc9c0b31504b2747cddba178ba8ce87007
Pallab Maji <5181716+codesteller@users.noreply.github.com>
2022-11-26 02:14:01 +0530

    Update jetson-setup.sh

--- jetson-setup.sh
+++ jetson-setup.sh
@@ -15,3 +15,16 @@ sudo jetson_clocks
 # Generate yolov3.onnx with the README shared in the /usr/src/tensorrt/samples/python/yolov3_onnx/.
 # Run the yolov3.onnx with trtexec, which targets for the profiling.
 /usr/src/tensorrt/bin/trtexec --onnx=./yolov3.onnx --workspace=26 --int8
+
+
+# Arducam Installation - https://www.arducam.com/docs/camera-for-jetson-nano/mipi-camera-modules-for-jetson-nano/driver-installation/
+mkdir -p apsoft/cam_drivers/arducam 
+cd apsoft/cam_drivers/arducam
+wget https://github.com/ArduCAM/MIPI_Camera/releases/download/v0.0.3/install_full.sh
+chmod +x install_full.sh
+./install_full.sh -m arducam
+
+
+
+
+
