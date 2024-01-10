225bbf690f9ab532d770b61d10517a8da68e4279
Pallab Maji <5181716+codesteller@users.noreply.github.com>
2024-01-11 00:43:59 +0530

    Update opencv_setup_jammy_4.8.1.sh

--- opencv_setup_jammy_4.8.1.sh
+++ opencv_setup_jammy_4.8.1.sh
@@ -6,32 +6,29 @@ cwd=$(pwd)
 sudo apt -y update
 sudo apt -y upgrade
 
-# Install Dependencies
 ## Install dependencies
 sudo apt -y install build-essential checkinstall cmake pkg-config yasm
 sudo apt -y install git gfortran
 sudo apt -y install libjpeg8-dev libpng-dev
  
 sudo apt -y install software-properties-common
-sudo add-apt-repository "deb http://security.ubuntu.com/ubuntu xenial-security main"
 sudo apt -y update
- 
-sudo apt -y install libjasper1
+
 sudo apt -y install libtiff-dev
  
-sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt -y install libavcodec-dev libavformat-dev libswscale-dev libdc1394-dev
 sudo apt -y install libxine2-dev libv4l-dev
 cd /usr/include/linux
 sudo ln -s -f ../libv4l1-videodev.h videodev.h
 cd "$cwd"
  
 sudo apt -y install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt -y install libgtk2.0-dev libtbb-dev qt5-default
+sudo apt -y install libgtk2.0-dev libtbb-dev qtcreator qtbase5-dev qt5-qmake
 sudo apt -y install libatlas-base-dev
 sudo apt -y install libfaac-dev libmp3lame-dev libtheora-dev
 sudo apt -y install libvorbis-dev libxvidcore-dev
 sudo apt -y install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt -y install libavresample-dev
+sudo apt -y install libavcodec-dev libavcodec-extra
 sudo apt -y install x264 v4l-utils
  
 # Optional dependencies
