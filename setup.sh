44712de60e8f9410e63700b72be01b5e370b1e4c
Pallab Maji <67859246+g-codesteller@users.noreply.github.com>
2024-01-24 22:11:22 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,3 +14,8 @@ sudo apt install apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.
   gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools \
   gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
 
+# Dev Utilities
+sudo apt install terminator tmux neofetch screen
+sudo snap install sublime-text --classic
+sudo snap install code --classic
+sudo snap install btop
