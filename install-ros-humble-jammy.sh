df1dd542eec43ad4bcf118a2ceeba9d0701791c3
Pallab Maji <67859246+g-codesteller@users.noreply.github.com>
2024-01-25 13:42:10 +0530

    Update install-ros-humble-jammy.sh

--- install-ros-humble-jammy.sh
+++ install-ros-humble-jammy.sh
@@ -1,9 +1,9 @@
 locale  # check for UTF-8
 
 sudo apt update && sudo apt install locales -y
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
+sudo locale-gen en_IN en_IN.UTF-8
+sudo update-locale LC_ALL=en_IN.UTF-8 LANG=en_IN.UTF-8
+export LANG=en_IN.UTF-8
 
 locale  # verify settings
 
@@ -15,7 +15,7 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt install ros-humble-desktop-full python3-argcomplete python-catkin-tools -y
-sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential -y
 
 # ROSDEP
 sudo rosdep init
