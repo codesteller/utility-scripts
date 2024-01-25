71ef8c9b2fb6534e97730ff5c7369825bf8fd8fe
Pallab Maji <67859246+g-codesteller@users.noreply.github.com>
2024-01-25 13:37:13 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,17 +5,17 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install build-essential software-properties-common git cmake 
+sudo apt install build-essential software-properties-common git cmake -y
 
 # Audio & Video Utils
-sudo apt install v4l-utils
-sudo apt install apt-get install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
+sudo apt install v4l-utils -y
+sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
   libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
   gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools \
-  gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+  gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio -y
 
 # Dev Utilities
-sudo apt install terminator tmux neofetch screen
+sudo apt install terminator tmux neofetch neovim -y
 sudo snap install sublime-text --classic
 sudo snap install code --classic
 sudo snap install btop
