9aa09b0fdf38b492aa44f10b302dfb5b90679e0c
Pallab Maji <67859246+g-codesteller@users.noreply.github.com>
2024-01-28 14:16:59 +0530

    Update setup_jammy.sh

--- setup_jammy.sh
+++ setup_jammy.sh
@@ -35,7 +35,7 @@ echo '    mamba activate' | tee -a ~/.bashrc > /dev/null
 echo '} ' | tee -a ~/.bashrc > /dev/null
 
 # Audio & Video Utils
-sudo apt install v4l-utils -y
+sudo apt install v4l-utils ffmpeg -y
 sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
   libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
   gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools \
