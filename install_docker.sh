441211c9d5c6afa226c0aa28ad64a403d48fc6f6
Pallab Maji <67859246+codesteller@users.noreply.github.com>
2025-08-09 15:51:45 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -20,3 +20,19 @@ sudo usermod -aG docker $USER
 
 # Test Docker
 sudo docker run --rm ubuntu:latest uname -a
+
+# # NVIDIA Docker for Ubuntu 22.04, Debian Buster
+# Add the package repositories
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+
+# Restart Docker
+sudo systemctl restart docker
+
+# Test nvidia-smi with the latest official CUDA image
+sudo docker run --gpus all nvcr.io/nvidia/cuda:latest nvidia-smi
+
