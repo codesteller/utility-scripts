931fb0c4881605626da8e642b654eb100f9218e8
Pallab Maji <67859246+codesteller@users.noreply.github.com>
2025-12-16 23:22:01 +0530

    Update setup script for additional packages and boot screen
    Added apt-transport-https to installation and changed boot screen.

--- setup_dbw_ecu_22.04.sh
+++ setup_dbw_ecu_22.04.sh
@@ -3,7 +3,7 @@
 cmake_version=3.31.5
 
 sudo apt update && sudo apt upgrade -y
-sudo apt install build-essential software-properties-common git curl wget -y
+sudo apt install build-essential software-properties-common git curl wget apt-transport-https -y
 
 # Download and Install CMake
 wget https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}-linux-x86_64.tar.gz
@@ -18,19 +18,22 @@ echo 'export PATH=/opt/cmake/${cmake_version}/bin:$PATH' >> ~/.bashrc
 sudo apt remove brltty -y
 
 # Dev Utilities
-sudo apt install terminator tmux neofetch neovim openssh-server -y
+sudo apt install terminator tmux neofetch neovim tmux openssh-server -y
 sudo snap install sublime-text --classic
 sudo snap install code --classic
-sudo snap install btop
+sudo snap install btop 
 
 # Base Python
 sudo apt install -y python3-dev python3-pip python3-venv
 
 # Audio & Video Utils
-sudo apt install -y v4l-utils ffmpeg
+sudo apt install -y v4l-utils ffmpeg 
 
 # Install ROS Humble
 curl -sL https://raw.githubusercontent.com/codesteller/utility-scripts/refs/heads/master/install-ros-humble-jammy.sh | bash
 
 # Install Docker
 curl -sL https://raw.githubusercontent.com/codesteller/utility-scripts/refs/heads/master/docker_setup_jammy.sh | bash
+
+# Change the Boot Screen 
+curl -sL https://raw.githubusercontent.com/codesteller/utility-scripts/refs/heads/master/change_boot_screen.sh | bash
