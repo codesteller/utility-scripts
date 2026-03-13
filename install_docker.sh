ca6badb25210316638f5c57b31a92518652464ca
Pallab Maji <67859246+codesteller@users.noreply.github.com>
2026-03-13 14:39:04 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -30,13 +30,15 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 
-#export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
+#export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.19.0-1
 #sudo apt-get install -y \
 #      nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
 #      nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
 #      libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
 #      libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
-      
+
+# Add to runtime
+sudo nvidia-ctk runtime configure --runtime=docker
 
 # Restart Docker
 sudo systemctl restart docker
