2b9c2939499a64cec0daa687755b3e9d8e57fba6
Pallab Maji <67859246+codesteller@users.noreply.github.com>
2026-03-13 12:36:49 +0530

    Update Docker installation script for NVIDIA toolkit

--- install_docker.sh
+++ install_docker.sh
@@ -28,8 +28,7 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
-sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
-sudo apt-get update
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 
 export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
 sudo apt-get install -y \
