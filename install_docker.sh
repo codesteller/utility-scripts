6b8df338e18b3281d1aa1d9c616d321013012366
Pallab Maji <67859246+codesteller@users.noreply.github.com>
2026-03-13 12:38:17 +0530

    Comment out NVIDIA container toolkit installation
    Comment out NVIDIA container toolkit installation commands.

--- install_docker.sh
+++ install_docker.sh
@@ -30,12 +30,12 @@ curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dear
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
 
-export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
-sudo apt-get install -y \
-      nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
-      nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
-      libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
-      libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
+#export NVIDIA_CONTAINER_TOOLKIT_VERSION=1.17.8-1
+#sudo apt-get install -y \
+#      nvidia-container-toolkit=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+#      nvidia-container-toolkit-base=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+#      libnvidia-container-tools=${NVIDIA_CONTAINER_TOOLKIT_VERSION} \
+#      libnvidia-container1=${NVIDIA_CONTAINER_TOOLKIT_VERSION}
       
 
 # Restart Docker
